679d833955fe45dec7272da714348023a2d87011
sanjayvp <svpselenium@gmail.com>
2020-10-04 15:01:11 +0530

    1st commit

---
+++ SampleProject/src/samplePackage/SampleClass.java
@@ -0,0 +1,12 @@
+package samplePackage;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("hi");
+
+	}
+
+}
