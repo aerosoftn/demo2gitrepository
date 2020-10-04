c20d8071a6c657a4d5dce29243a427e996e7c250
sanjayvp <svpselenium@gmail.com>
2020-10-04 12:58:30 +0530

    first time

---
+++ Demo2/src/test/java/pkg1/LoginTest.java
@@ -0,0 +1,45 @@
+package pkg1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest{
+	WebDriver driver;
+	@Test
+	public void setUp1() {
+		System.out.println("opening chrome driver");
+		
+	WebDriverManager.chromedriver().setup();
+	
+		driver=new ChromeDriver();
+	
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		System.out.println("opening site");
+		driver.get("https://www.google.com/");
+	}
+	@Test
+	public void setUp2() {
+		System.out.println("opening chrome driver");
+		
+	WebDriverManager.chromedriver().setup();
+	
+		driver=new ChromeDriver();
+	
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		System.out.println("opening site");
+		driver.get("https://stackoverflow.com/");
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+}
