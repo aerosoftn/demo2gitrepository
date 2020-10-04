39573668fcb7307776ff8d46423d2f359e78f285
sanjayvp <svpselenium@gmail.com>
2020-10-04 15:51:43 +0530

    code changed in main method

--- SampleProject/src/SampleClass.java
+++ SampleProject/src/SampleClass.java
@@ -3,7 +3,8 @@ public class SampleClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sample Class");
+		System.out.println("Sample Class1");
+		System.out.println("Sample Class2");
 
 	}
 
